c4c57be146beb8f391d42807f5081261fe4e92e4
GerashchenkoDmitry <finiqw@gmail.com>
2021-08-30 12:35:17 +0700

    Initial Commit

---
+++ SignIn/SignInApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignInApp.swift
+//  SignIn
+//
+//  Created by Дмитрий Геращенко on 30.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct SignInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
